9658d6314a1a35fc5a34561df0d6613bf86e6e5a
tarangvats <63825607+tarangvats@users.noreply.github.com>
2021-10-11 18:04:44 +0530

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,1 +1,20 @@
+class Quizbrain:
+    question_number = 0
+    def __init__(self,q_list):
+        self.list = q_list
+        self.question_number=0
+        self.correct =0
+    def next_question(self):
+        current_question = self.list[self.question_number]
+        self.question_number+=1
+        answer = input(f"Q.{self.question_number} : {current_question.question} ")
+        if answer==current_question.correct_answer:
+            print("You got it right!")
+            self.correct+=1
+        else:
+            print("incorrect")
+
+        print(f"Correct answer was {current_question.correct_answer}")
+        print(f"Your current score is: {self.correct}/{self.question_number}")
+
 
