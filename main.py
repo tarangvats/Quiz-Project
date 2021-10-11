4b8b38590548fe67c307497ad9a19f2edabd8efc
tarangvats <63825607+tarangvats@users.noreply.github.com>
2021-10-11 18:03:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import Quizbrain
 
+
+
+
+question_bank = []
+for i in question_data:
+    q1 = i["question"]
+    a1 = i["correct_answer"]
+    q = Question(q1,a1)
+    question_bank.append(q)
+quiz = Quizbrain(question_bank)
+for question in question_bank:
+
+    quiz.next_question()
+
+print("You have completed your quiz")
+print(f"Your final score was :{quiz.correct}/{len(question_bank)}")
